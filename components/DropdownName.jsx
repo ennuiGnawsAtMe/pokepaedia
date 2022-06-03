c0e8a530f8041916749bf57988d33124a1babc11
Luke <luke@rpo-global.com>
2022-06-03 12:57:22 +1200

    m

--- components/DropdownName.jsx
+++ components/DropdownName.jsx
@@ -3,15 +3,7 @@ import Select from 'react-select'
 import { getPokemonAsync } from '../lib/controllers'
 import styles from '../styles/DropdownName.module.css'
 
-<<<<<<< HEAD
-
-// COMPONENT
-const DropdownName = ({ names, setSelection}) => {
-||||||| 6bb0c2b
-const DropdownName = ({ names, setSelection}) => {
-=======
 const DropdownName = ({ setSelection}) => {
->>>>>>> ed985a7df4de46aa40972f34cd91feb6ad69d2c5
 
   // const getNames = () => {
   //   const options = names.map(poke => {
