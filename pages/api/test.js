3438c57e789aadeb08a719d6e5177e845b3bc385
Luke <luke@rpo-global.com>
2022-06-15 10:26:53 +1200

    testing api

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,7 +1,14 @@
+import prisma from "../../lib/prisma"
 
 const handler = async (req, res) => {
 
-    return res.status(200).json("Hello World!")
+    const pokemon = await prisma.pokemon.findUnique({
+        where: {
+            pokedex: 1
+        }
+    })
+
+    return res.status(200).json(pokemon)
 }
 
 export default handler
