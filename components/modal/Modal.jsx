fc502783dd04b4f4a0567400af3bbe9d332a64e5
luke-concannon <luke_conker@hotmail.com>
2022-06-24 21:26:07 +1200

    modal working, not finished

--- components/modal/Modal.jsx
+++ components/modal/Modal.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react"
 import Image from "next/image"
+import ReactPortal from "../utils/ReactPortal"
 import styles from './Modal.module.css'
 import ReactStars from "react-rating-stars-component"
 
@@ -15,30 +16,32 @@ const Modal = ({ ratingOverall, totalRatings, name, pokedex, blurb, image, isOpe
   if (!isOpen) return null
 
   return (
-    <div className={styles.modal} >
-      <button onClick={() => handleClose()} className={styles.closeBtn}>
-              Close
-            </button>
-      <div className={styles.modalContent}>
-          <div className={styles.imageContainer}>
-            <Image src={image} alt={name} layout="fill" objectFit='contain' />
-          </div>
-        <div className={styles.detailsWrapper}>
-            <h1>{name}</h1>
-            <ReactStars size={20} value={ratingOverall} edit={false} isHalf={true} />
-            <h4>{ratingOverall} stars from {totalRatings} ratings</h4>
-            <p>{blurb}</p>
-            <ReactStars size={50} value={0} onChange={newValue => changeHandler(newValue)} />
-          <form action="/api/ratings" method="post">
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" />
-            <label htmlFor="comment">Comment:</label>
-            <input type="text" id="comment" name="comment" />
-            <button type="submit">Submit</button>
-          </form>
-        </div> 
+    <ReactPortal wrapperId="react-portal-modal-container">
+      <div className={styles.modal} >
+        <button onClick={() => handleClose()} className={styles.closeBtn}>
+                Close
+              </button>
+        <div className={styles.modalContent}>
+            <div className={styles.imageContainer}>
+              <Image src={image} alt={name} layout="fill" objectFit='contain' />
+            </div>
+          <div className={styles.detailsWrapper}>
+              <h1>{name}</h1>
+              <ReactStars size={20} value={ratingOverall} edit={false} isHalf={true} />
+              <h4>{ratingOverall} stars from {totalRatings} ratings</h4>
+              <p>{blurb}</p>
+              <ReactStars size={50} value={0} onChange={newValue => changeHandler(newValue)} />
+            <form action="/api/ratings" method="post">
+              <label htmlFor="name">Name:</label>
+              <input type="text" id="name" name="name" />
+              <label htmlFor="comment">Comment:</label>
+              <input type="text" id="comment" name="comment" />
+              <button type="submit">Submit</button>
+            </form>
+          </div> 
+        </div>
       </div>
-    </div>
+    </ReactPortal>
   )
 }
 export default Modal
---
+++ components/utils/ReactPortal.jsx
@@ -0,0 +1,42 @@
+import { createPortal } from 'react-dom'
+import { useState, useLayoutEffect } from 'react'
+
+const createWrapperAndAppendToBody = (wrapperId) => {
+      const wrapperElement = document.createElement('div')
+      wrapperElement.setAttribute("id", wrapperId)
+      document.body.appendChild(wrapperElement)
+      return wrapperElement
+    }
+
+const ReactPortal = ({ children, wrapperId = "react-portal-wrapper" }) => {
+    const [wrapperElement, setWrapperElement] = useState(null)
+
+    useLayoutEffect(() => {
+    let element = document.getElementById(wrapperId);
+    let systemCreated = false;
+
+    if (!element) {
+      systemCreated = true;
+      element = createWrapperAndAppendToBody(wrapperId);
+    }
+    setWrapperElement(element)
+
+    return () => {
+      if (systemCreated && element.parentNode) {
+        element.parentNode.removeChild(element);
+      }
+    }
+  }, [wrapperId])
+
+  if (wrapperElement === null) return null
+
+    let element = document.getElementById(wrapperId)
+
+    if (!element) {
+        element = createWrapperAndAppendToBody(wrapperId)
+    }
+
+    return createPortal(children, element)
+}
+
+export default ReactPortal
