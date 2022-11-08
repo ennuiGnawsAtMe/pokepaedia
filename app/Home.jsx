99fb1cbc3ca2ae4619367016813d841299bc5482
luke-concannon <luke@rpo-manufacturing.com>
2022-11-08 22:30:22 +1300

    some initial styling

--- app/Home.jsx
+++ app/Home.jsx
@@ -1,16 +1,16 @@
 
 const Home = ({ pokemon }) => {
-    return (
-        <div>
-          {pokemon.map(poke => (
-            <div key={poke.pokedex}>
-              <h1 className="font-bold underline text-3xl" >{poke.name}</h1>
-              <h2 >{poke.ranking}</h2>
-            </div>
-          )
-           )}
+  return (
+    <div className="flex flex-row justify-center flex-wrap">
+      {pokemon.map(poke => (
+        <div className="p-4 bg-slate-800 rounded-md	m-2 text-center	flex flex-col justify-between	" key={poke.pokedex}>
+          <h1 className="font-normal underline text-3xl text-white	" >{poke.name}</h1>
+          <h2 className="text-white">{poke.ranking}</h2>
         </div>
-    )
+      )
+      )}
+    </div>
+  )
 }
 
 export default Home
--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,12 @@
-import './globals.css'
+import './globals.css';
 
 // Layouts must accept a children prop.
 // This will be populated with nested layouts or pages
-  
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="w-screen">{children}</body>
     </html>
-  )
+  );
 }
