c1abd50b728b5396b24cf3a51e4c8c56e48deebc
luke-concannon <luke@rpo-manufacturing.com>
2022-11-29 22:36:29 +1300

    added heroicons and headless ui

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -1,12 +1,16 @@
 import Nav from './Nav'
 import Welcome from './Welcome'
 import SearchList from '../../components/utils/SearchList'
+import Dropdown from './Dropdown'
 
 const Page = () => {
   return (
     <>
       <div className="top-0 flex min-h-[10vh] w-screen flex-row items-center justify-between bg-gradient-to-r from-blue-900 to-blue-400 shadow-lg shadow-red-500">
-        <Nav />
+        <div className="flex flex-col">
+          <Nav />
+          <Dropdown />
+        </div>
         <Welcome />
       </div>
       <SearchList />
