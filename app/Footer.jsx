e7fe7915f2072f8625819e3dcc88a9106feafb86
luke-concannon <luke@rpo-manufacturing.com>
2023-01-19 20:28:56 +1300

    right amount of pokemon in footer

--- app/Footer.jsx
+++ app/Footer.jsx
@@ -3,13 +3,13 @@ import Image from 'next/image'
 
 const Footer = () => {
   const shuffled = Object.values(pokemonImages).sort(() => 0.5 - Math.random())
-  const selected = shuffled.slice(0, 10)
+  const selected = shuffled.slice(0, 7)
 
   return (
     <div className="flex w-[100vw] shrink-0 flex-row justify-center">
       {selected.map((pokemon, index) => (
         <Image
-          className=" w-[10vw] duration-200 ease-in-out hover:-translate-y-4 active:-translate-y-2"
+          className="-ml-[2vw] w-[15vw] duration-200 ease-in-out hover:-translate-y-4 active:-translate-y-2"
           alt="Gotta catch em all!"
           key={index}
           src={pokemon}
